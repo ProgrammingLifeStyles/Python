c609b8c88e37130fd9265a9941e3e0f0d5ef75d8
ProgrammingLifeStyles <54428745+ProgrammingLifeStyles@users.noreply.github.com>
2019-08-22 21:58:04 -0700

    Add files via upload

---
+++ Lesson One - Hello World.py
@@ -0,0 +1,14 @@
+"""
+In Pythin when you want to display text to the screen you must use a command.
+More specifically the print() command.
+"""
+
+#Example - You must have quotation marks around the text
+print("I am a print command")
+
+#Practice - Type your name in the print command
+print("")
+
+"""
+Wow! you did it, in the next leason you will be learning what variables are and how to use them.
+"""
