ce5446ae0bd2a5c8d847aac9671709e3396fff87
ProgrammingLifeStyles <54428745+ProgrammingLifeStyles@users.noreply.github.com>
2019-08-24 12:10:59 -0700

    Add files via upload

---
+++ Lesson Two - Variables.py
@@ -0,0 +1,24 @@
+"""
+Lesson Two - Variables
+
+In this lesson you will learn
+how to use variables
+"""
+
+# This is a Variable
+n = 1
+
+"""
+Variables are used when you want to store info quickly
+the way you use this stored info is like this
+"""
+
+#Example 1
+n = "Hello World!"
+
+print(n)
+
+#Practice 1 - type your name inside the quotes
+n = ""
+
+print("My name is" + n)
