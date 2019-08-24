9f91a3517858fd733f78d6cfe3ea806d99bbd544
ProgrammingLifeStyles <54428745+ProgrammingLifeStyles@users.noreply.github.com>
2019-08-24 12:53:57 -0700

    Add files via upload

---
+++ Lesson Four D - Input().py
@@ -0,0 +1,27 @@
+"""
+Lesson Four - Input()
+
+In this lesson you will learn
+how to take user input and how to use it
+"""
+
+#Example 1 - This takes the user input
+
+    #input("")
+    
+
+#Example 2 - You can then store this data in a variable
+
+    #n = input("")
+
+#Example 3 - You can also display text with input
+
+n = input("What is your name? ")
+    
+print("Hello " + n)
+
+#Practice 1 - Write your own questian and display it
+
+q = input("")
+
+print("") #Tip - dont forget to add the variable to the print line
