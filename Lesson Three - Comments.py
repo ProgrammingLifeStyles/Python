260263b99fb338f9b74c58d6b6bf061f85d4bb5e
ProgrammingLifeStyles <54428745+ProgrammingLifeStyles@users.noreply.github.com>
2019-08-24 12:32:33 -0700

    Add files via upload

---
+++ Lesson Three - Comments.py
@@ -0,0 +1,31 @@
+"""
+Lesson Three - Comments
+
+In this lesson you will learn
+how to use the two types of comments
+in python
+"""
+
+#Example 1 - single line comment
+
+    #This is a single line comment, It can go anywhere and the code will look past it
+
+#Practice 1 - Type your name in the comment
+
+    #
+
+#Example 2 - Multi-line comment
+    
+    """
+    I
+    am
+    a
+    Multi-line
+    comment
+    """
+    
+#Practice 2 - Write your self a multi-line comment
+    
+    """
+    Type here
+    """
