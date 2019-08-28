a603fa057d60b9fac830c4f1f72f3e14b75f87cd
ProgrammingLifeStyles <54428745+ProgrammingLifeStyles@users.noreply.github.com>
2019-08-27 22:14:57 -0700

    Update Lesson Four D - Input().py

--- Lesson Four D - Input().py
+++ Lesson Four D - Input().py
@@ -20,7 +20,11 @@ n = input("What is your name? ")
     
 print("Hello " + n)
 
-#Practice 1 - Write your own questian and display it
+#Example 4 - Numbers
+    #when you want your input to be a number you must wrap it in an int()
+    #int(input("Like this"))
+
+#Practice 1 - Write your own questian and display it the do the same thing but ask for a number
 
 q = input("")
 
